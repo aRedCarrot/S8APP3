0e40fe12bec18865c7a277a65e6d83035d4fffed
ClaudeGP <31514003+ClaudeGP@users.noreply.github.com>
2022-11-03 23:03:12 -0400

    Fix tripwire

--- Docker/LogsAPI/tripwireExec.sh
+++ Docker/LogsAPI/tripwireExec.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-no| apt-get -y install tripwire
-cat SettingTripWire.txt >> /etc/tripwire/twpol.txt
-twadmin --create-polfile -s -Q "" -S /etc/tripwire/site.key /etc/tripwire/twpol.txt && tripwire --init -P ""
-
-cd /app
+cat SettingTripWire.txt >> /etc/tripwire/twpol.txt && twadmin --create-polfile -s -Q "" -S /etc/tripwire/site.key /etc/tripwire/twpol.txt && tripwire --init -P ""
